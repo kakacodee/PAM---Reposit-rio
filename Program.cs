0aed4ab440e6d648c1035af888b24c0c456cd033
kakacodee <kakas13.souza@gmail.com>
2024-03-19 18:50:46 -0300

    criação do código em c#(1 commit)

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pamex___Samuel_e_Kauã
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira seu nome: ");
+           string nome = Console.ReadLine();
+            Console.WriteLine("Olá" + nome + "seja bem-vindo!!");
+        }
+    }
+}
