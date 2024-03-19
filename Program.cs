34848c6f9cecf2977a9096add04c06f121dbd95b
kakacodee <kakas13.souza@gmail.com>
2024-03-19 19:09:59 -0300

    continuação do código em c#(2 commit)

--- Program.cs
+++ Program.cs
@@ -10,9 +10,25 @@ namespace Pamex___Samuel_e_Kauã
     {
         static void Main(string[] args)
         {
+            double num1, num2, total;
+            string nome;
             Console.Write("Insira seu nome: ");
-           string nome = Console.ReadLine();
-            Console.WriteLine("Olá" + nome + "seja bem-vindo!!");
+            nome = Console.ReadLine();
+            Console.WriteLine("Olá " + nome + ", seja bem-vindo!!");
+            Console.ReadKey();
+
+            Console.Write("Digite um número: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.ReadLine();
+            Console.Write("Digite outro número: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+            Console.ReadLine();
+
+            total = num1 + num2;
+            Console.log("Somando " + num1 + " com " + num2 + " o resultado será: " + total);
+
+
+
         }
     }
 }
